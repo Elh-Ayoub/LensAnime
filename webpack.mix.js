21302e9427fb7a036f34d6a02e44cb55cd6f0cef
Ayoub El-Haddadi <ayoub1998elh@gmail.com>
2021-09-18 23:06:54 +0300

    add AdminLTE and create a readme

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,8 +10,5 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.copyDirectory('node_modules/admin-lte/dist', 'public/dist');
+ mix.copyDirectory('node_modules/admin-lte/plugins', 'public/plugins');
